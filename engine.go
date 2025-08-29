de2ee220305478a6244493c6a62d95a626f5c2bf
Doron Cohen <4966182+doron-cohen@users.noreply.github.com>
2025-08-29 21:32:21 +0200

    refactor(engine): remove unecessary workflow

--- engine.go
+++ engine.go
@@ -4,39 +4,53 @@ import (
 	"context"
 )
 
-// Engine is the main engine for the workflow.
+type (
+	EventKey string
+	Action   func(ctx context.Context, data any) error
+)
+
+// Engine maps events to actions and executes them.
 type Engine struct {
-	workflows map[string]Workflow
+	// actions maps event keys to their corresponding actions
+	actions map[EventKey]Action
 }
 
-// NewEngine creates a new workflow engine.
+// NewEngine creates a new event engine.
 func NewEngine() *Engine {
 	return &Engine{
-		workflows: make(map[string]Workflow),
+		actions: make(map[EventKey]Action),
 	}
 }
 
-// On creates a new workflow builder for the given event key.
-func (e *Engine) On(eventKey string) *WorkflowBuilder {
-	return NewWorkflowBuilder(eventKey, e.addWorkflow)
+// On registers an action for the given event keys.
+func (e *Engine) On(eventKeys ...EventKey) *ActionBuilder {
+	return &ActionBuilder{
+		engine:    e,
+		eventKeys: eventKeys,
+	}
 }
 
-// Send sends an event to the engine which will trigger the relevant workflows.
-// It returns true if the event was sent, false if no workflows are registered
-// for the event.
-func (e *Engine) Send(ctx context.Context, eventKey string, data any) bool {
-	workflow, ok := e.workflows[eventKey]
+// Send sends an event to the engine which will trigger the registered action.
+// It returns true if the event was sent, false if no action is registered for the event.
+func (e *Engine) Send(ctx context.Context, eventKey EventKey, data any) bool {
+	action, ok := e.actions[eventKey]
 	if !ok {
 		return false
 	}
 
-	go workflow.Run(ctx, data)
-
+	go action(ctx, data)
 	return true
 }
 
-func (e *Engine) addWorkflow(eventKey string, workflow Workflow) error {
-	e.workflows[eventKey] = workflow
+// ActionBuilder builds actions for events.
+type ActionBuilder struct {
+	engine    *Engine
+	eventKeys []EventKey
+}
 
-	return nil
+// Do registers the action for all the event keys.
+func (ab *ActionBuilder) Do(action Action) {
+	for _, eventKey := range ab.eventKeys {
+		ab.engine.actions[eventKey] = action
+	}
 }
--- engine_test.go
+++ engine_test.go
@@ -3,6 +3,7 @@ package waffle_test
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -14,14 +15,12 @@ func TestEngine_Send(t *testing.T) {
 	ran := false
 
 	engine := waffle.NewEngine()
-	err := engine.
-		On("test").
-		Do(func(_ context.Context, _ any) error {
-			ran = true
-			return nil
-		}).
-		Build()
-	require.NoError(t, err)
+
+	// Register action for event
+	engine.On("test").Do(func(_ context.Context, _ any) error {
+		ran = true
+		return nil
+	})
 
 	started := engine.Send(t.Context(), "test", nil)
 	require.True(t, started)
@@ -35,19 +34,15 @@ func TestEngine_SendWithData(t *testing.T) {
 	data := ""
 
 	engine := waffle.NewEngine()
-	err := engine.
-		On("test").
-		Do(func(_ context.Context, d any) error {
-			var ok bool
-			data, ok = d.(string)
-			if !ok {
-				return fmt.Errorf("expected string, got %T", d)
-			}
 
-			return nil
-		}).
-		Build()
-	require.NoError(t, err)
+	engine.On("test").Do(func(_ context.Context, d any) error {
+		var ok bool
+		data, ok = d.(string)
+		if !ok {
+			return fmt.Errorf("expected string, got %T", d)
+		}
+		return nil
+	})
 
 	started := engine.Send(t.Context(), "test", "some data")
 	require.True(t, started)
@@ -55,4 +50,26 @@ func TestEngine_SendWithData(t *testing.T) {
 	time.Sleep(100 * time.Millisecond)
 
 	require.Equal(t, "some data", data)
+}
+
+func TestEngine_SendMultiple(t *testing.T) {
+	counter := atomic.Int32{}
+
+	engine := waffle.NewEngine()
+
+	engine.On("test").Do(func(_ context.Context, _ any) error {
+		counter.Add(1)
+		return nil
+	})
+
+	// Send multiple events
+	ran1 := engine.Send(t.Context(), "test", nil)
+	ran2 := engine.Send(t.Context(), "test", nil)
+
+	require.True(t, ran1)
+	require.True(t, ran2)
+
+	time.Sleep(100 * time.Millisecond)
+
+	require.Equal(t, int32(2), counter.Load())
 }
